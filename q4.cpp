df11aad3565b0d54d77e2ce7bc3a1f233ae6ce39
DebasmitBiswal <debasmitbiswal@outlook.com>
2023-01-04 22:06:51 +0530

    Added 4ans

---
+++ q4.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/rotate-function/description/
+// Microsoft
+
+class Solution
+{
+public:
+    int maxRotateFunction(vector<int> &nums)
+    {
+        int size(nums.size());
+        int cur{};
+        for (int i = 1; i < size; i++)
+        {
+            cur += i * nums[i];
+        }
+
+        int prev(cur), sum(accumulate(nums.begin(), nums.end(), 0));
+        int maxi(cur);
+        for (int i = size - 1; i > 0; i--)
+        {
+            cur = prev + sum - (size * nums[i]);
+            prev = cur;
+            maxi = max(maxi, cur);
+        }
+        return maxi;
+    }
+};
